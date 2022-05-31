82eb2eabf682549077ad3fb50a33e2f78a11178d
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-05-31 14:27:51 +0300

    awdad f wf w

--- src/components/TheAside.vue
+++ src/components/TheAside.vue
@@ -1,70 +1,55 @@
 <template>
-  <aside class="aside bg-aside-color d-flex flex-column">
+  <aside class="aside bg-aside-color p-3 d-flex flex-column">
     <div class>
       <div class="logo">
         <h3 class="text-primary fw-bold fs-3 text-center mb-3">YourVictory</h3>
       </div>
       <div>
-        <ul class="p-0 list-unstyled mb-0">
+        <ul class="p-0 list-unstyled">
           <li class="w-100">
-            <router-link
-              to="/home"
-              class="aside-link p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-3 text-decoration-none justify-content-center justify-content-lg-start"
-              exact-active-class="aside-link--active"
+            <a
+              href="#"
+              class="p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-md-3 text-decoration-none"
             >
-              <div class="me-lg-2">
-                <BaseSystemIcons name="mainpage" />
-              </div>
-              <span class="d-lg-inline d-none">Главная</span>
-            </router-link>
+              <div class="me-2"><BaseSystemIcons name="mainpage"/></div>
+              <span class="text-primary fs-7">awdawd add</span>
+            </a>
           </li>
           <li class="w-100">
-            <router-link
-              to="/user-page"
-              class="aside-link p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-3 text-decoration-none justify-content-center justify-content-lg-start"
-              exact-active-class="aside-link--active"
+            <a
+              href="#"
+              class="p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-md-3 text-decoration-none"
             >
-              <div class="me-lg-2">
-                <BaseSystemIcons name="check" />
-              </div>
-              <span class="d-lg-inline d-none">awdawd add</span>
-            </router-link>
+              <div class="me-2"><BaseSystemIcons name="check"/></div>
+              <span class="text-primary fs-7">awdawd add</span>
+            </a>
           </li>
           <li class="w-100">
-            <router-link
-              to="#"
-              class="aside-link p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-3 text-decoration-none justify-content-center justify-content-lg-start"
-              exact-active-class="aside-link--active"
+            <a
+              href="#"
+              class="p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-md-3 text-decoration-none"
             >
-              <div class="me-lg-2">
-                <BaseSystemIcons name="analytics" />
-              </div>
-              <span class="d-lg-inline d-none">awdawd add</span>
-            </router-link>
+              <div class="me-2"><BaseSystemIcons name="analytics"/></div>
+              <span class="text-primary fs-7">awdawd add</span>
+            </a>
           </li>
           <li class="w-100">
-            <router-link
-              to="#"
-              class="aside-link p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-3 text-decoration-none justify-content-center justify-content-lg-start"
-              exact-active-class="aside-link--active"
+            <a
+              href="#"
+              class="p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 mb-md-3 text-decoration-none"
             >
-              <div class="me-lg-2">
-                <BaseSystemIcons name="page" />
-              </div>
-              <span class="d-lg-inline d-none">awdawd add</span>
-            </router-link>
+              <div class="me-2"><BaseSystemIcons name="page"/></div>
+              <span class="text-primary fs-7">awdawd add</span>
+            </a>
           </li>
           <li class="w-100">
-            <router-link
-              to="#"
-              class="aside-link p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 text-decoration-none justify-content-center justify-content-lg-start"
-              exact-active-class="aside-link--active"
+            <a
+              href="#"
+              class="p-3 shadow-sm d-flex align-items-center border border-primary rounded-3 text-decoration-none"
             >
-              <div class="me-lg-2">
-                <BaseSystemIcons name="app" />
-              </div>
-              <span class="d-lg-inline d-none">awdawd add</span>
-            </router-link>
+              <div class="me-2"><BaseSystemIcons name="app"/></div>
+              <span class="text-primary fs-7">awdawd add</span>
+            </a>
           </li>
         </ul>
       </div>
@@ -73,7 +58,5 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
